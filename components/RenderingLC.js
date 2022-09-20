c7ac5747583b546128b71baca335ecaf389601e8
Ahkal <bp@sojourn.llc>
2022-09-20 17:07:17 -0400

    omments out layouteffect

--- components/RenderingLC.js
+++ components/RenderingLC.js
@@ -5,19 +5,27 @@ const RenderingLC = ({ title }) => {
     console.log("useEffect 1");
   });
 
-  useLayoutEffect(() => {
-    console.log("useLayoutEffect 1");
-  });
+  // useLayoutEffect(() => {
+  //   console.log("useLayoutEffect 1");
+  // });
 
-  useLayoutEffect(() => {
-    console.log("useLayoutEffect 2");
-  });
+  // useLayoutEffect(() => {
+  //   console.log("useLayoutEffect 2");
+  // });
 
   useEffect(() => {
     console.log("useEffect 2");
   });
 
   console.log("rendering");
+
+  // OUTPUT:
+  // --
+  // rendering
+  // useLayoutEffect 1 RenderingLC.js:9:12
+  // useLayoutEffect 2 RenderingLC.js:13:12
+  // useEffect 1 RenderingLC.js:5:12
+  // useEffect 2
 
   return (
     <>
